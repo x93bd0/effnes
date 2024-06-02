08257339feb8ae3895fd15f1c5c59c485057be0f
Edoardo Borgia Leiva <edoardo.borgia.leiva@outlook.com>
2024-06-02 13:53:09 +0200

    Minor formatting in mapper/mapper.h

--- mappers/mapper.h
+++ mappers/mapper.h
@@ -11,24 +11,24 @@ typedef struct Mapper {
 } Mapper;
 
 typedef char* MapperInf;
-MapperInf	MI_fetch		(char* ROM);
+MapperInf	MI_fetch	(char* ROM);
 
 // iNES flags
-uint8_t		MI_isINES		(MapperInf INF);
-uint16_t	MI_mapper		(MapperInf INF);
-uint8_t		MI_nmarg		(MapperInf INF);
+uint8_t		MI_isINES	(MapperInf INF);
+uint16_t	MI_mapper	(MapperInf INF);
+uint8_t		MI_nmarg	(MapperInf INF);
 
-uint8_t		MI_prgrom		(MapperInf INF);
-uint8_t		MI_chrrom		(MapperInf INF);
-uint8_t		MI_prgram		(MapperInf INF);
+uint8_t		MI_prgrom	(MapperInf INF);
+uint8_t		MI_chrrom	(MapperInf INF);
+uint8_t		MI_prgram	(MapperInf INF);
 
 uint8_t		MI_battery	(MapperInf INF);
 uint8_t		MI_trainer	(MapperInf INF);
-uint8_t		MI_altnml		(MapperInf INF);
-uint8_t		MI_isVSU		(MapperInf INF);
-uint8_t		MI_isPLC		(MapperInf INF);
-uint8_t		MI_isNTSC		(MapperInf INF);
-uint8_t		MI_isPAL		(MapperInf INF);
+uint8_t		MI_altnml	(MapperInf INF);
+uint8_t		MI_isVSU	(MapperInf INF);
+uint8_t		MI_isPLC	(MapperInf INF);
+uint8_t		MI_isNTSC	(MapperInf INF);
+uint8_t		MI_isPAL	(MapperInf INF);
 uint8_t		MI_haspram	(MapperInf INF);
 uint8_t		MI_busconf	(MapperInf INF);
 
@@ -38,6 +38,6 @@ uintmx_t	MI_fpiroff	(MapperInf INF);
 uintmx_t	MI_fprmoff	(MapperInf INF);
 uintmx_t	MI_fsenoff	(MapperInf INF);
 
-void			MI_destroy	(MapperInf INF);
+void		MI_destroy	(MapperInf INF);
 
 #endif
